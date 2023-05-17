5dd871374db9f3dcb9a43b7d6036bcb9ef950f11
Jude Wells <judewells@hotmail.com>
2023-05-18 00:38:41 +0100

    update submit script

--- submit_scripts/dockstring.qsub.sh
+++ submit_scripts/dockstring.qsub.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 #$ -l tmem=3.9G
 #$ -l h_vmem=3.9G
-#$ -l h_rt=01:55:30
+#$ -l h_rt=00:54:00
 #$ -S /bin/bash
-#$ -N docking
-#$ -t 1-192
-#$ -wd /SAN/orengolab/nsp13/dock_pdbbind/random_scripts
+#$ -N dude
+#$ -t 1-5
+#$ -wd /SAN/orengolab/nsp13/dude/cluster_dockstring
 # EXAMPLE SUBMISSION COMMAND:
+# qsub /SAN/orengolab/nsp13/dude/cluster_dockstring/submit_scripts/dockstring.qsub.sh
 #These are optional flags but you probably want them in all jobs
 #$ -j y
+
+echo "$@"
+# args_file=$1
+# entrypoint=$2
+extra_args="${@:2}"  # e.g. batch size
+
 hostname
 date
-PROJECT_HOME=/SAN/orengolab/nsp13/shared
-source $PROJECT_HOME/source_files/vina.source
-source $PROJECT_HOME/source_files/conda.source
-conda activate cheminfo2
-cd /SAN/orengolab/nsp13/dock_pdbbind/random_scripts
+conda activate dockstring
+cd /SAN/orengolab/nsp13/dude/cluster_dockstring
 # UPDATE FOR YOUR ENVIRONMENT
-/SAN/orengolab/nsp13/.conda/envs/cheminfo2/bin/python3 vina_dock_ligand.py ${SGE_TASK_ID}
+/SAN/orengolab/nsp13/.conda/envs/dockstring/bin/python vina_dock_ligand.py --task_index ${SGE_TASK_ID} --smiles_file dude_data/selected_dude/drd3/actives_final.ism --target drd3 --active_decoy active
 date
--- test_docking.py
+++ test_docking.py
@@ -1,22 +1,28 @@
 import sys
+import argparse
 sys.path.append('/SAN/orengolab/nsp13/dude/dockstring')
 
 from dockstring import load_target
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Dock batch of ligands to a target."
+    )
+    parser.add_argument("--task_index", type=int)
+    parser.add_argument("--target", type=str)
+    parser.add_argument("--active_decoy", type=str)
+    parser.add_argument("--smiles_file", type=str)
+    args = parser.parse_args()
     batch_size = 100
-    task_index = int(sys.argv[1]) -1
-    smiles_file = sys.argv[2]
-    target = sys.argv[3]
-    active_decoy = sys.argv[4]
-    with open(smiles_file, "r") as f:
+    task_index = args.task_index - 1
+    with open(args.smiles_file, "r") as f:
         smiles_lines = f.readlines()[task_index*batch_size:(task_index+1)*batch_size]
     for i, line in enumerate(smiles_lines):
-        if active_decoy == "active":
+        if args.active_decoy == "active":
             smiles, _, lig_id = line.split()
         else:
             smiles, lig_id = line.split()
-        working_directory = f"/SAN/orengolab/nsp13/dude/outputs_dockstring/{target}/{active_decoy}/{lig_id}/"
-        target = load_target(target, working_dir=working_directory)
+        working_directory = f"/SAN/orengolab/nsp13/dude/outputs_dockstring/{args.target}/{args.active_decoy}/{lig_id}/"
+        target = load_target(args.target, working_dir=working_directory)
         score, _ = target.dock(smiles)
         print(f"Docking was successful, score={score:.3g}. END OF SCRIPT.")
